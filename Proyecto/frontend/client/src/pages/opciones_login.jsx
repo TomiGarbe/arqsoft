b7dd8d327133d66acc029aa927c67539ed88e00a
ossanaTomas <ossanatomas5@gmail.com>
2023-06-09 13:11:43 -0300

    A

--- Proyecto/frontend/client/src/pages/opciones_login.jsx
+++ Proyecto/frontend/client/src/pages/opciones_login.jsx
@@ -3,16 +3,34 @@ import './opciones_login.css';
 
 const RoleSelection = () => {
   return (
+    <div>    
+       <div className="header">
+            <h2 className="titulo-header">TuHotel.com</h2>
+       </div>
+
+
     <div className="selection">
-      <div className="buttons-container">
-        <button className="button" variant="contained" size="large">
-          <a className="link" href="http://localhost:3000/login_cliente">Cliente</a>
-        </button>
-        <button className="button" variant="contained" size="large">
-          <a className="link" href="http://localhost:3000/login_admin">Administrador</a>
-        </button>
-      </div>
-    </div>
+      
+        <div className="seleccion-container">
+         
+            <h2 className="titulo">Seleccione según corresponda</h2>
+            <div className="buttons-container">
+              <button className="button cliente" variant="contained" size="large">
+                <a className="link" href="http://localhost:3000/login_cliente">Cliente</a>
+              </button>
+              <button className="button administrador" variant="contained" size="large">
+                <a className="link" href="http://localhost:3000/login_admin">Administrador</a>
+              </button>
+            </div>
+          
+        </div>
+       </div>
+       <div className="footer">
+        fotter
+       </div>
+       </div> 
+       
+    
   );
 };
 
